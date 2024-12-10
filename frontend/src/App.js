a2f1983d9abf87efe021d95cf357d32fd7d1be7c
Atakan Akın <atakanakink@icloud.com>
2024-12-10 21:22:30 +0300

    Revert "[frontend] Google Sign-in"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,10 @@ import MainPage from "./components/MainPage";
 import CreatePage from "./components/CreatePage";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import DashboardPage from "./components/DashboardPage";
-import {LoginPage} from "./components/LoginPage";
-import {PrivateRoute} from "./components/PrivateRoute";
-import { AuthProvider } from "./context/AuthContext";
 import './i18n';
 
 const App = () => {
     return (
-        <AuthProvider>
         <Router>
             <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
                 <Header />
@@ -20,20 +15,13 @@ const App = () => {
                     <Routes>
                         {/* Define routes for the main page and the create page */}
                         <Route path="/" element={<MainPage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/create" element={<PrivateRoute>
-                <CreatePage />
-              </PrivateRoute>} />
-                        <Route path="/dashboard" element={<PrivateRoute>
-                <DashboardPage />
-              </PrivateRoute>} />
+                        <Route path="/create" element={<CreatePage />} />
                         <Route path="*" element={<Navigate to="/" />} />
                     </Routes>
                 </div>
                 <Footer />
             </div>
         </Router>
-        </AuthProvider>
     );
 };
 
