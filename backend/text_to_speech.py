7595eb923db3fd83a833ddab268253d6c58404d4
Atakan Akın <atakan.akin@commencis.com>
2025-01-13 21:04:44 +0300

    [backend] Improve text-to-speech; closes #24

--- backend/text_to_speech.py
+++ backend/text_to_speech.py
@@ -4,6 +4,7 @@ from langdetect import detect
 import asyncio
 import random
 
+
 def text_to_speech(input_path: str, output_dir: str) -> tuple[str, str]:
     """
     Convert text content to speech and word-level subtitles.
@@ -22,33 +23,53 @@ def text_to_speech(input_path: str, output_dir: str) -> tuple[str, str]:
 
     with open(input_path, "r", encoding="utf-8") as file:
         text = file.read()
-    
-    lang = detect(text)
+
+    try:
+        lang = detect(text)
+    except:
+        lang = "multilang"
+
     voices = asyncio.run(get_voices(lang))
-        
-    communicate = Communicate(text = text, voice = random.choice(voices)["Name"])
+
+    communicate = Communicate(text=text, voice=random.choice(voices)["Name"])
     submaker = SubMaker()
-    
+
     async def save_files():
         with open(audio_output_path, "wb") as file:
             async for chunk in communicate.stream():
                 if chunk["type"] == "audio":
                     file.write(chunk["data"])
                 elif chunk["type"] == "WordBoundary":
-                    submaker.create_sub((chunk["offset"], chunk["duration"]), chunk["text"].upper())
+                    submaker.create_sub(
+                        (chunk["offset"], chunk["duration"]), chunk["text"].upper()
+                    )
         with open(subtitle_output_path, "w", encoding="utf-8") as file:
             file.write(submaker.generate_subs(words_in_cue=1))
-            
+
     asyncio.run(save_files())
-    
+
     return audio_output_path, subtitle_output_path
-    
+
 
 async def get_voices(lang: str) -> list:
     vm = await VoicesManager.create()
+    locale = "en-US"
+    base_voices = vm.find(Locale=locale)
+    base_voices = [
+        voice
+        for voice in base_voices
+        if "Cute" not in voice["VoiceTag"]["VoicePersonalities"]
+    ]
     if lang == "en":
-        locale = "en-US"
-        voices = vm.find(Locale = locale)
-        return voices
-    voices = vm.find(Gender = "Male", Language = lang)
+        return base_voices
+    multiple_lang_voices = [
+        voice for voice in base_voices if "Multilingual" in voice["Name"]
+    ]
+    if lang == "multilang":
+        return multiple_lang_voices
+    voices = vm.find(Language=lang)
+    # add multilingual voices and avoid duplicates
+    for voice in multiple_lang_voices:
+        if voice not in voices:
+            voices.append(voice)
     return voices
